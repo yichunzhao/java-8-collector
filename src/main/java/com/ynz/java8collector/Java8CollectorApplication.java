373d7700e374936b749983b3e46b2a3d75b7349d
yichunzhao <zhaoyichun@hotmail.com>
2020-10-04 17:58:49 +0200

    refactoring

--- src/main/java/com/ynz/java8collector/Java8CollectorApplication.java
+++ src/main/java/com/ynz/java8collector/Java8CollectorApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Comparator;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -36,69 +37,31 @@ public class Java8CollectorApplication implements CommandLineRunner {
         log.info("total number employees: " + String.valueOf(company.getAllEmployees().size()));
 
         log.info("+++++ find out the most common employee name in the company. +++++");
-        company.getAllEmployees()
-                .stream()
-                .collect(Collectors.groupingBy(Employee::getName))
-                .entrySet()
-                .stream()
-                .sorted((e1, e2) -> Integer.compare(e2.getValue().size(), e1.getValue().size()))
-                .findFirst()
-                .ifPresent(entry -> log.info(entry.toString()));
 
-        log.info("+++++ find the highest salaried employee of the company. +++++");
+        //find max/min element in a Stream using max(Comparator)/min(Comparator)
         company.getAllEmployees()
                 .stream()
-                .sorted((e1, e2) -> e2.getSalary().compareTo(e1.getSalary()))
-                .findFirst()
-                .ifPresent(employee -> log.info("Employee with the highest salary: " + employee.toString()));
-
-        //using Collectors.maxBy()/minBy() to do the same as the above;
-        company.getAllEmployees()
-                .stream()
-                .collect(Collectors.maxBy(Comparator.comparingDouble(employee -> employee.getSalary().doubleValue())))
+                .max((e1, e2) -> e1.getSalary().compareTo(e2.getSalary()))
                 .ifPresent(employee -> log.info("most salaried employee: " + employee));
 
         log.info("+++++ Find the sum of salaries for all men. +++++");
-        Double totalSalary = company.getAllEmployees().stream()
-                .mapToDouble(employee -> employee.getSalary().doubleValue()).sum();
+        double totalSalary = company.getAllEmployees()
+                .stream()
+                .mapToDouble(employee -> employee.getSalary().doubleValue())
+                .sum();
         log.info("total salary: " + totalSalary);
-
-        //using Collector.summingDouble to do the same
-        Double sumSalary = company.getAllEmployees().stream()
-                .collect(Collectors.summingDouble(employee -> employee.getSalary().doubleValue()));
-        log.info("total salary: " + sumSalary);
 
 
         log.info("+++++ Find the most popular grade/band in the company. +++++");
-        company.getAllEmployees().stream()
-                .collect(Collectors.groupingBy(employee -> employee.getBand()))
-                .entrySet()
-                .stream()
-                .sorted((b1, b2) -> Integer.compare(b2.getValue().size(), b1.getValue().size()))
-                .findFirst()
-                .ifPresent(bandListEntry -> log.info("The Most popular band: " + bandListEntry.toString()));
 
-        company.getAllEmployees().stream()
-                .collect(Collectors.groupingBy(employee -> employee.getBand()))
-                .entrySet()
+        //find a max/min in a Map
+        //Collectors.groupingBy(r=f(t),Collectors) directly counting the number in the list
+        company.getAllEmployees()
                 .stream()
-                .collect(Collectors.maxBy((b1, b2) -> Integer.compare(b1.getValue().size(), b2.getValue().size())))
-                .ifPresent(bandListEntry -> log.info("The Most popular band: " + bandListEntry.toString()));
-
-        company.getAllEmployees().stream()
-                .collect(Collectors.groupingBy(employee -> employee.getBand()))
-                .entrySet()
-                .stream()
-                .collect(Collectors.maxBy(Comparator.comparingInt(bandListEntry -> bandListEntry.getValue().size())))
-                .ifPresent(bandListEntry -> log.info("The Most popular band: " + bandListEntry.toString()));
-
-        company.getAllEmployees().stream()
                 .collect(Collectors.groupingBy(Employee::getBand, Collectors.counting()))
                 .entrySet()
                 .stream()
-                .collect(Collectors.maxBy((b1, b2) -> Long.compare(b1.getValue(), b2.getValue())))
-                .ifPresent(bandLongEntry -> log.info("The Most popular band: " + bandLongEntry.toString()));
-
-
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .ifPresent(bandListEntry -> log.info("??The Most popular band is " + bandListEntry.getKey() + " having " + bandListEntry.getValue() + " members"));
     }
 }
